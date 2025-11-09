b93531d66fae890db2d68d1b27eded84e7fcaa6a
Peter kanyori <peterynaenah23@gmail.com>
2025-11-09 10:31:00 +0300

    Plantsy

--- src/components/PlantPage.js
+++ src/components/PlantPage.js
@@ -1,14 +1,41 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 import NewPlantForm from "./NewPlantForm";
 import PlantList from "./PlantList";
 import Search from "./Search";
 
 function PlantPage() {
+  const [plants, setPlants] = useState([]);
+  const [search, setSearch] = useState("");
+
+  // Fetch all plants
+  useEffect(() => {
+    fetch("http://localhost:6001/plants")
+      .then((r) => r.json())
+      .then(setPlants);
+  }, []);
+
+  // Add new plant
+  function handleAddPlant(newPlant) {
+    setPlants([...plants, newPlant]);
+  }
+
+  // Toggle sold-out status
+  function handleToggleSoldOut(id) {
+    setPlants(plants.map((p) =>
+      p.id === id ? { ...p, soldOut: !p.soldOut } : p
+    ));
+  }
+
+  // Filter by search
+  const filteredPlants = plants.filter((plant) =>
+    plant.name.toLowerCase().includes(search.toLowerCase())
+  );
+
   return (
     <main>
-      <NewPlantForm />
-      <Search />
-      <PlantList />
+      <NewPlantForm onAddPlant={handleAddPlant} />
+      <Search search={search} onSearchChange={setSearch} />
+      <PlantList plants={filteredPlants} onToggleSoldOut={handleToggleSoldOut} />
     </main>
   );
 }
